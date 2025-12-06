9bf2ca4da563bc125208b9451c45be9a3466e2f3
Hasham <hasham.khalid@codup.co>
2025-12-07 00:03:46 +0500

    build(gadget): upgrade to gadget v.15

--- api/models/session/schema.gadget.ts
+++ api/models/session/schema.gadget.ts
@@ -4,7 +4,7 @@ import { type GadgetModel } from 'gadget-server';
 // For more information on how to update this file http://docs.gadget.dev
 
 export const schema: GadgetModel = {
-  type: 'gadget/model-schema/v1',
+  type: 'gadget/model-schema/v2',
   storageKey: '-SiIvIMl8PAD',
   fields: {
     roles: {
@@ -13,5 +13,6 @@ export const schema: GadgetModel = {
       storageKey: 'OfMjtHVwaQNN',
     },
   },
-  shopify: { fields: ['shop', 'shopifySID'] },
+  searchIndex: false,
+  shopify: { fields: { shop: true, shopifySID: true } },
 };
